f2be0d283ea3b948d4e14e4299d3753b007fe789
Amare <mxamare@gmail.com>
2026-08-11 22:47:11 -0400

    test

--- src/pages/Make.jsx
+++ src/pages/Make.jsx
@@ -5,3 +5,10 @@ import content from '../data/content.json';
 export default function Make() {
   return <PageLayout {...content.make} />;
 }
+// fix footer
+// add static intro to code entris
+// add color
+// intro and tech stack at top, iteration view on the side.
+// below are iterations as blog entries. 
+// different smaller font
+// bett looing hero
